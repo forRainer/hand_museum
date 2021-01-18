78d78051fbb7738833d5f26b84b823fa2281a94d
forRainer <1173106641@qq.com>
2021-01-18 16:47:10 +0800

    初始代码

---
+++ pages/demo/demo.js
@@ -0,0 +1,86 @@
+// pages/demo/demo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg: "fuck you",
+    list: [
+      {
+        id: 0,
+        data: "aaa"
+      },
+      {
+        id: 1,
+        data: "bbb"
+      }
+    ],
+    user: {
+      age: 18,
+      hight: 1.7
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  jumpTo: function (e) {
+    wx.navigateTo({
+      url: '/pages/demo1/demo1'
+    })
+  }
+})
---
+++ pages/demo1/demo1.js
@@ -0,0 +1,142 @@
+// pages/demo1/demo1.js
+const bgMusic = wx.createInnerAudioContext()
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isOpen: false,//播放开关
+    starttime: '00:00', //正在播放时长
+    duration: '01:00',   //总时长
+    src: "media/demo.mp3"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    var that = this
+    that.listenerButtonStop()//停止播放
+    console.log("离开")
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  // 播放
+  listenerButtonPlay: function (e) {
+    var that = this;
+    //bug ios 播放时必须加title 不然会报错导致音乐不播放
+    bgMusic.title = '此时此刻'  
+    bgMusic.epname = '此时此刻'
+    bgMusic.src = that.data.src;
+    console.log(1111)
+    bgMusic.onTimeUpdate(() => { 
+      //bgMusic.duration总时长  bgMusic.currentTime当前进度
+      console.log(2222)
+      console.log(bgMusic.currentTime)
+      var duration = bgMusic.duration; 
+      var offset = bgMusic.currentTime;  
+      var currentTime = parseInt(bgMusic.currentTime);
+      var min = "0" + parseInt(currentTime / 60);
+      var max = parseInt(bgMusic.duration);
+      var sec = currentTime % 60;
+      if (sec < 10) {
+        sec = "0" + sec;
+      };
+      var starttime = min + ':' + sec;   /*  00:00  */
+      that.setData({
+        offset: currentTime,
+        starttime: starttime,
+        max: max,
+        changePlay: true
+      })
+    })
+    //播放结束
+    bgMusic.onEnded(() => {
+      that.setData({
+        starttime: '00:00',
+        isOpen: false,
+        offset: 0
+      })
+      console.log("音乐播放结束");
+    })
+    console.log(222);
+    bgMusic.play();
+    console.log(bgMusic.duration);
+    that.setData({
+      isOpen: true,
+    })
+  },
+  //暂停播放
+  listenerButtonPause(){
+     var that = this
+    bgMusic.pause()
+    that.setData({
+      isOpen: false,
+    })
+  },
+  listenerButtonStop(){
+    var that = this
+    bgMusic.stop()
+  },
+  // 进度条拖拽
+  sliderChange(e) {
+    var that = this
+    var offset = parseInt(e.detail.value);
+    bgMusic.play();
+    bgMusic.seek(offset);
+    that.setData({
+      isOpen: true,
+    })
+  }
+})
